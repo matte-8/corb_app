9955e2b0e9f348cdc041020094b75b16e304080c
matte-8 <abbb.biii.biii@gmail.com>
2025-06-11 16:48:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
 function toggleMenu() {
-  const menu = document.getElementById('popupMenu');
-  menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+  const menu = document.getElementById("popupMenu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
